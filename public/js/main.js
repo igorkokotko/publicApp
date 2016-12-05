3732905e2d8cca4cd2982e7933d582dcf7807083
Ihor <thylacosmilu@gmail.com>
2016-12-05 22:36:57 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,44 @@
-"use strict";
+(function() {
 
-const onPow = (number) => {
-  let k = 0;
-  let kPow = Math.pow(4, k);
- while (number > kPow) {
- kPow = Math.pow(4, k++);
-  }
-  return k - 2;
-};
+  let simpleApp = angular.module("simpleApp", []);
 
-document.getElementById("task1").innerHTML = onPow(1025);
+  simpleApp.controller('TaskController', function() {
+    let vm = this;
+    vm.onPow = number => {
+      if (number <= 1 || !number) {
+        return 0;
+      }
+      else {
+        let k = 0;
+        let kPow = Math.pow(4, k);
+        while (number > kPow) {
+          kPow = Math.pow(4, k++);
+        }
+        return k-2;
+      }
+    };
 
-const congruenceOfSquares = (num) => {
-  const result = +((Math.sqrt(num / 2)).toFixed(2));
-  return result;
-};
+    vm.congruenceOfSquares = num => {
+      if (!num) {
+        return 0;
+      }
+      else {
+        return +((Math.sqrt(num / 2)).toFixed(2));
+      }
+    };
 
-document.getElementById("task2").innerHTML = congruenceOfSquares(162);
 
-const allSquaredPairs = (num) => {
-  const pairs = [];
-  for (let i = 0; i <= Math.sqrt(num / 2); i++) {
-    const j = Math.sqrt(num - i * i);
-    if (j % 1 == 0) {
-      pairs.push([j, i]);
-    }
-  }
-  return pairs;
-};
+    vm.allSquaredPairs = (num) => {
+      const pairs = [];
+      for (let i = 0; i <= Math.sqrt(num / 2); i++) {
+        const j = Math.sqrt(num - i * i);
+        if (j % 1 == 0) {
+          pairs.push([j, i]);
+        }
+      }
+      return pairs;
+    };
+  });
 
-document.getElementById("task3").innerHTML = allSquaredPairs(25);
+}());
+
